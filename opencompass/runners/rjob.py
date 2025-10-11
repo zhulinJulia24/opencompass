1e1f07a6bcd682cc90b48889652419fc74abc861
zhulin1 <zhulinJulia24@163.com>
2025-10-11 13:58:41 +0800

    update

--- opencompass/runners/rjob.py
+++ opencompass/runners/rjob.py
@@ -115,10 +115,6 @@ class RJOBRunner(BaseRunner):
                     status = 'Timeout'
                     found_dict = True
                     break
-                if 'Inqueue' in line:
-                    status = 'Inqueue'
-                    found_dict = True
-                    break
                 if 'Restarting' in line:
                     status = 'Restarting'
                     found_dict = True
