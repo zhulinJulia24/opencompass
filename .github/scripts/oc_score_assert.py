e620b276b6c5dfad8234f235ed6797d50fc2530c
zhulin1 <zhulinJulia24@163.com>
2025-10-30 13:19:00 +0800

    update

--- .github/scripts/oc_score_assert.py
+++ .github/scripts/oc_score_assert.py
@@ -120,7 +120,7 @@ class TestChatFullbench:
     def test_chat_longtext(self, baseline_scores_fullbench, result_scores,
                            model, dataset):
         base_score = baseline_scores_fullbench.get(model).get(
-            'chat_longtext_fullbench').get(dataset)
+            'chat_longtext').get(dataset)
         result_score = result_scores.get(model).get(dataset)
         assert_score(model, result_score, base_score, dataset)
 
