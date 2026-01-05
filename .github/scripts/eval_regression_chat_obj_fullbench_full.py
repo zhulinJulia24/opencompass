59f1967aa477db68daa73d8c275cb3641258a645
zhulin1 <zhulinJulia24@163.com>
2026-01-05 16:18:45 +0800

    update

--- .github/scripts/eval_regression_chat_obj_fullbench_full.py
+++ .github/scripts/eval_regression_chat_obj_fullbench_full.py
@@ -6,42 +6,25 @@ from opencompass.utils.text_postprocessors import extract_non_reasoning_content
 
 with read_base():
     # Datasets
-    from opencompass.configs.datasets.atlas.atlas_val_gen_b2d1b6 import \
-        atlas_datasets  # noqa: F401, E501
-    from opencompass.configs.datasets.biodata.biodata_task_gen import \
-        biodata_task_datasets  # noqa: F401, E501
-    from opencompass.configs.datasets.CMPhysBench.cmphysbench_gen import \
-        cmphysbench_datasets  # noqa: F401, E501
-    from opencompass.configs.datasets.IFBench.IFBench_gen import \
-        ifbench_datasets  # noqa: F401, E501
-    from opencompass.configs.datasets.livecodebench_pro.livecodebench_pro_gen import \
-        lcb_pro_datasets  # noqa: F401, E501
-    from opencompass.configs.datasets.MolInstructions_chem.mol_instructions_chem_gen import \
-        mol_gen_selfies_datasets  # noqa: F401, E501
-    from opencompass.configs.datasets.openswi.openswi_gen import \
-        openswi_datasets  # noqa: F401, E501
+    from opencompass.configs.datasets.SciReasoner.scireasoner_gen import (  # noqa: F401, E501
+        mini_bio_instruction_datasets, mini_composition_material_datasets,
+        mini_GUE_datasets, mini_LLM4Mat_datasets,
+        mini_modulus_material_datasets, mini_mol_biotext_datasets,
+        mini_mol_mol_datasets, mini_mol_protein_datasets, mini_opi_datasets,
+        mini_PEER_datasets, mini_Retrosynthesis_uspto50k_datasets,
+        mini_smol_datasets, mini_UMG_Datasets, mini_uncond_material_datasets,
+        mini_uncond_protein_datasets, mini_uncond_RNA_datasets)
 
     from ...rjob import eval, infer  # noqa: F401, E501
 
-datasets = sum((v for k, v in locals().items() if k.endswith('_datasets')), [])
+datasets = [
+    v[0] for k, v in locals().items()
+    if k.endswith('_datasets') and isinstance(v, list) and len(v) > 0
+]
 
 for d in datasets:
     if 'n' in d:
         d['n'] = 1
-    if 'reader_cfg' in d:
-        d['reader_cfg']['test_range'] = '[0:16]'
-    else:
-        d['test_range'] = '[0:16]'
-    if 'eval_cfg' in d and 'dataset_cfg' in d['eval_cfg'][
-            'evaluator'] and 'reader_cfg' in d['eval_cfg']['evaluator'][
-                'dataset_cfg']:
-        d['eval_cfg']['evaluator']['dataset_cfg']['reader_cfg'][
-            'test_range'] = '[0:16]'
-    if 'eval_cfg' in d and 'llm_evaluator' in d['eval_cfg'][
-            'evaluator'] and 'dataset_cfg' in d['eval_cfg']['evaluator'][
-                'llm_evaluator']:
-        d['eval_cfg']['evaluator']['llm_evaluator']['dataset_cfg'][
-            'reader_cfg']['test_range'] = '[0:16]'
 
 hf_model = dict(type=HuggingFacewithChatTemplate,
                 abbr='qwen-3-8b-hf-fullbench',
